e729ad32726508c45e71d827b1796cbd2411779b
Aashish0217 <aashish.chauhan0217@gmail.com>
2022-10-16 21:00:12 +0530

    Conditional_Operator

---
+++ Web23-Conditional-Statements/problem2.js
@@ -0,0 +1,6 @@
+let age=20;
+if (age>=18){
+  console.log("Apply for licence");
+}else {
+  console.log("NA");
+}
---
+++ Web23-Conditional-Statements/problem4.js
@@ -0,0 +1,16 @@
+//User data stored in database 
+let database_username = "217ash";
+let database_password = "ashish";
+
+//For login
+let username = "217ash";
+let password = "ashish";
+
+if (database_username==username){
+  if (database_password==password){
+    console.log ("Login successfully");
+  }else ("Password incorrect");
+}
+else{
+("wrong credentials") ; 
+} 
